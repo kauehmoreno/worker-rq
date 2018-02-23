4762ddeb1cab5a165ceba1fd70b2dc2ecea4d785
Kaueh Moreno <kaueh.rodrigues@corp.globo.com>
2018-02-23 17:48:42 -0300

    main.go: Log definition on initializer

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
+import log "github.com/sirupsen/logrus"
+
+func init() {
+	log.SetFormatter(&log.JSONFormatter{})
+	log.SetLevel(log.WarnLevel)
+}
+
 func main() {
 	var img ImageBucket
 	img.Consumer()
